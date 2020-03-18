43e1acfcabe723af8520c92b5ed48d7eba12a5cc
tianzhi <1095098525@qq.com>
2020-03-18 23:19:46 +0800

    webapck

--- server/src/container/Home/index.js
+++ server/src/container/Home/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+// 同构：一套react代码，在服务器端执行一次，再客户端执行一次
+
 const Home = () => {
     return (
         <div>
-            首页
+            首页1234
         </div>
     )
 }
--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,16 @@ import Home from './container/Home/index'
 import React from 'react'
 import {renderToString} from 'react-dom/server'
 
+// 虚拟dom是一个真实Dom的javascript对象映射
+// ReactDOM.render(<Home/>, document.getElementById("root"))
+
+// 客户端渲染：react代码在客户端运行，消耗的是浏览器的性能
+// 服务端渲染弊端：react代码在服务器端运行，消耗的是服务器的性能，极大的消耗服务端资源
+// 服务端渲染优势：1、首屏加载速度快 2、有利于seo
+
 const app = express();
 // 将home组件渲染成字符串，直接返回给浏览器，这样在浏览器可以查看到htmt内容 形成简易的服务端渲染
+// renderToString只能将基本的信息转成字符串 点击事件等无法生效 所以需要同构
 const content = renderToString(<Home />)
 
 app.get('/', function(req, res) {

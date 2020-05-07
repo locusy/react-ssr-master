ec32ee953db28f3e7de5aff76a5b172ca6d4f061
tianzhi <1095098525@qq.com>
2020-05-07 17:56:38 +0800

    modify

--- server/src/container/NotFound/index.js
+++ server/src/container/NotFound/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 export default class NotFound extends Component {
     componentWillMount() {
         // 只有服务端才有 在render之前context会自动加上NOT_FOUNED属性
--- server/src/container/Translation/index.js
+++ server/src/container/Translation/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from "react-router-dom";
 
+
 export default class Translation extends Component {
     render() {
         return (
